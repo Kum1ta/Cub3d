05ddfc5ed47285c31d53db5f339831cd7370a328
Paul Salame <psalame@student.42angouleme.fr>
2024-03-11 22:06:24 +0100

    struct v1

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: edbernar <edbernar@student.42angouleme.    +#+  +:+       +#+        */
+/*   By: psalame <psalame@student.42angouleme.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/10 14:58:17 by edbernar          #+#    #+#             */
-/*   Updated: 2024/03/10 14:58:49 by edbernar         ###   ########.fr       */
+/*   Updated: 2024/03/11 22:06:15 by psalame          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,38 @@
 # define CUB3D_H
 
 # include "libft.h"
+# include <stdbool.h>
+
+
+typedef enum e_block {
+	WALL,
+	FLOOR,
+	EMPTY,
+	END
+}	t_block;
+
+typedef struct s_texture {
+	char	*north;
+	char	*south;
+	char	*east;
+	char	*west;
+
+	int		floor[3];
+	int		ceiling[3];
+}	t_texture;
+
+typedef struct s_vec3 {
+	float	x;
+	float	y;
+	float	z;
+	float	h;
+}	t_vec3;
+
+typedef struct s_map {
+	t_block		**blocks;
+	
+	t_texture	texture;
+	t_vec3		playerPos;
+}	t_map;
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: edbernar <edbernar@student.42angouleme.    +#+  +:+       +#+        */
+/*   By: psalame <psalame@student.42angouleme.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/10 14:57:39 by edbernar          #+#    #+#             */
-/*   Updated: 2024/03/10 15:13:04 by edbernar         ###   ########.fr       */
+/*   Updated: 2024/03/11 21:53:42 by psalame          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,13 @@
 
 int	main(int ac, char **av)
 {
+	t_block map[5][5] = {
+		{ WALL , WALL , WALL , WALL , END },
+		{ WALL , FLOOR, FLOOR, WALL , END },
+		{ WALL , WALL , FLOOR , WALL , END },
+		{ EMPTY, WALL , WALL , WALL , END },
+		NULL
+	};
 	(void)ac;
 	(void)av;
 	ft_printf("Hello world !");
