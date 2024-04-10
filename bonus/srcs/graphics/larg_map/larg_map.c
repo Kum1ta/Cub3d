2ffd00d867ff7972108dd7a7081d5c4f610f3409
edbernar <edbernar@3F2.42angouleme.fr>
2024-04-10 20:35:55 +0200

    change larg map

--- bonus/srcs/graphics/larg_map/larg_map.c
+++ bonus/srcs/graphics/larg_map/larg_map.c
@@ -3,19 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   larg_map.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: psalame <psalame@student.42angouleme.fr    +#+  +:+       +#+        */
+/*   By: edbernar <edbernar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 14:39:07 by edbernar          #+#    #+#             */
-/*   Updated: 2024/04/10 17:01:34 by psalame          ###   ########.fr       */
+/*   Updated: 2024/04/10 20:35:30 by edbernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../graphics.h"
 
-void	draw_square(t_mlx *mlx, int size, int color, int x, int y)
+void	draw_square(t_mlx *mlx, int size, int color, int x, int y, void *img)
 {
-	int		i;
-	int		j;
+	int	i;
+	int	j;
 
 	i = 0;
 	while (i < size)
@@ -23,8 +23,8 @@ void	draw_square(t_mlx *mlx, int size, int color, int x, int y)
 		j = 0;
 		while (j < size)
 		{
-			if (x + i < mlx->stg->width && y + j < mlx->stg->height && x + i > 0 && y + j > 0)
-				mlx_pixel_put(mlx->mlx, mlx->win, x + i, y + j, color);
+			if (x + i >= 0 && x + i < mlx->stg->width && y + j >= 0 && y + j < mlx->stg->height)
+				mlx_set_image_pixel(mlx->mlx, img, x + i, y + j, color);
 			j++;
 		}
 		i++;
@@ -47,58 +47,98 @@ void	update_pos(t_mlx *mlx)
 	mlx->mouse->last_y = mlx->mouse->y;
 }
 
-void	calculate_target_position(t_mlx *mlx, t_vec3 camDir, float distance)
+
+void	fill_img_map(t_mlx *mlx, void *img)
 {
-	t_vec2	target_pos;
+	int	i;
+	int	j;
+	int	x;
+	int	y;
 
-	target_pos.x = mlx->menu_map->x
-		+ mlx->menu_map->size * mlx->map->playerPos.x
-		+ mlx->menu_map->size / 4 + distance * camDir.x;
-	target_pos.y = mlx->menu_map->y
-		+ mlx->menu_map->size * mlx->map->playerPos.y
-		+ mlx->menu_map->size / 4 + distance * camDir.y;
-	mlx_pixel_put(mlx->mlx, mlx->win, target_pos.x, target_pos.y, 0xFFFF0000);
-}
-
-void	print_map(t_mlx *mlx)
-{
-	int				i;
-	int				j;
-	int				x;
-	int				y;
-
-	y = 0;
 	i = 0;
-	while (mlx->map->blocks[i] && y < mlx->stg->height)
+	y = mlx->menu_map->y;
+	while (i < mlx->map->height)
 	{
 		j = 0;
-		x = 0;
-		while (mlx->map->blocks[i][j].type != END && x <= mlx->stg->width)
+		x = mlx->menu_map->x;
+		while (mlx->map->blocks[i][j].type != END)
 		{
-			x = j * mlx->menu_map->size + mlx->menu_map->x;
-			y = i * mlx->menu_map->size + mlx->menu_map->y;
 			if (mlx->map->blocks[i][j].type == WALL)
-				draw_square(mlx, mlx->menu_map->size, 0x55F0000F, x, y);
+				draw_square(mlx, mlx->menu_map->size, 0xFF646464, x - 5, y - 5, img);
+			else if (mlx->map->blocks[i][j].type == DOOR)
+				draw_square(mlx, mlx->menu_map->size, 0xFF989898, x - 5, y - 5, img);
+			else if (mlx->map->blocks[i][j].type == FLOOR)
+				draw_square(mlx, mlx->menu_map->size, 0xFF323232, x, y, img);
+			x += mlx->menu_map->size;
 			j++;
 		}
-		mlx->menu_map->width = j * mlx->menu_map->size;
+		y += mlx->menu_map->size;
 		i++;
 	}
-	mlx->menu_map->height = i * mlx->menu_map->size;
-	draw_square(mlx, 5, 0xFFFF0000, mlx->map->playerPos.x * mlx->menu_map->size + mlx->menu_map->x,
-		mlx->map->playerPos.y * mlx->menu_map->size + mlx->menu_map->y);
+}
+
+void	fill_image(t_mlx *mlx, void *img, int color, int xy[2])
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < xy[0])
+	{
+		j = 0;
+		while (j < xy[1])
+		{
+			mlx_set_image_pixel(mlx->mlx, img, i, j, color);
+			j++;
+		}
+		i++;
+	}
+}
+
+
+void	down_bar(t_mlx *mlx, int need_free)
+{
+	static void	*img = NULL;
+
+	if (need_free)
+	{
+		if (img)
+			mlx_destroy_image(mlx->mlx, img);
+		return ;
+	}
+	if (!img)
+	{
+		img = mlx_new_image(mlx->mlx, mlx->stg->width, 100);
+		fill_image(mlx, img, 0xFF212121, (int [2]){mlx->stg->width, 100});
+	}
+	mlx_put_image_to_window(mlx->mlx, mlx->win, img, 0, mlx->stg->height - 100);
 }
 
 void	larg_map(t_mlx *mlx, int need_free)
 {
+	static void	*img = NULL;
+
 	if (need_free)
+	{
+		down_bar(mlx, 1);
+		if (img)
+			mlx_destroy_image(mlx->mlx, img);
 		return ;
+	}
+	if (img)
+	{
+		mlx_destroy_image(mlx->mlx, img);
+		img = NULL;
+	}
+	if (!img)
+		img = mlx_new_image(mlx->mlx, mlx->stg->width, mlx->stg->height);
 	update_pos(mlx);
-	calculate_target_position(mlx, mlx->map->camDir,
-		mlx->menu_map->size);
-	print_map(mlx);
-	mlx_string_put(mlx->mlx, mlx->win, mlx->stg->width - 300, 40, 0xFFFFFFFF,
+	fill_img_map(mlx, img);
+	mlx_put_image_to_window(mlx->mlx, mlx->win, img, 0, 0);
+	mlx_string_put(mlx->mlx, mlx->win, 10, 50, 0xFFFFFFFF,
 		"'tab' to return to the game");
-	mlx_string_put(mlx->mlx, mlx->win, mlx->stg->width - 300, 50, 0xFFFFFFFF,
+	mlx_string_put(mlx->mlx, mlx->win, 10, 80, 0xFFFFFFFF,
 		"'r' to reset the map");
+	down_bar(mlx, 0);
+	mlx_string_put(mlx->mlx, mlx->win, 50, mlx->stg->height - 40, 0xFFFFFFFF, "Resume");
 }
