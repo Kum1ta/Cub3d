d0e47ab92988890d0310f2f26492fa3b2cec9dc8
Paul Salame <psalame@student.42angouleme.fr>
2024-04-23 17:58:57 +0200

    fixed network issue (can't see player some times)

--- bonus/srcs/networking/server/req_actions/set_bonus.c
+++ bonus/srcs/networking/server/req_actions/set_bonus.c
@@ -6,39 +6,39 @@
 /*   By: psalame <psalame@student.42angouleme.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 14:56:30 by psalame           #+#    #+#             */
-/*   Updated: 2024/04/20 16:19:20 by psalame          ###   ########.fr       */
+/*   Updated: 2024/04/23 17:58:04 by psalame          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../server_Int_bonus.h"
 #define PLAYER_ATTR_REQ "setPlayerAttr:%d,%s,%.2f,%.2f,%.2f,%.2f,%.2f,%d;"
 
-void	act_init_player(t_server *srv, int client_i, char *value)
+void	act_init_player(t_server *srv, int cl_i, char *value)
 {
 	int			i;
-	const char	*base_str = value;
+	const char	*old = value;
+	t_client	ply;
 
 	i = 0;
 	while (*value && *value != ',')
 		if (i <= SV_MAX_PLAYER_NAME)
-			srv->clients[client_i].player_name[i++] = *value++;
-	srv->clients[client_i].player_name[i] = 0;
+			srv->clients[cl_i].player_name[i++] = *value++;
+	srv->clients[cl_i].player_name[i] = 0;
 	while (*value++ != ',')
 		;
-	srv->clients[client_i].player_pos = parse_vec3(&value);
-	srv->clients[client_i].player_dir = parse_vec2(&value);
+	srv->clients[cl_i].player_pos = parse_vec3(&value);
+	srv->clients[cl_i].player_dir = parse_vec2(&value);
+	srv->clients[cl_i].health = ft_atoi(value);
 	i = -1;
 	while (++i < SV_MAX_CONNECTION)
 	{
-		if (i == client_i || srv->clients[i].socket == -1)
+		if (i == cl_i || srv->clients[i].socket == -1)
 			continue ;
-		ft_dprintf(srv->clients[i].socket, "setPlayerAttr:%d,%s;",
-			client_i, base_str);
-		dprintf(srv->clients[client_i].socket, PLAYER_ATTR_REQ,
-			i, srv->clients[i].player_name, srv->clients[i].player_pos.x,
-			srv->clients[i].player_pos.y, srv->clients[i].player_pos.z,
-			srv->clients[i].player_dir.x, srv->clients[i].player_dir.y,
-			srv->clients[i].health);
+		ply = srv->clients[i];
+		ft_dprintf(srv->clients[i].socket, "setPlayerAttr:%d,%s;", cl_i, old);
+		dprintf(srv->clients[cl_i].socket, PLAYER_ATTR_REQ, i, ply.player_name,
+			ply.player_pos.x, ply.player_pos.y, ply.player_pos.z,
+			ply.player_dir.x, ply.player_dir.y, ply.health);
 	}
 }
 
